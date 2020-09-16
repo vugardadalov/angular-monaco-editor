c4cbf33c9e826d88637fad0cb92e1d72104bd096
vugar_dadalov <vuqar.ahmedoglu@gmail.com>
2020-09-16 21:22:09 +0400

    ng zorro add, editor module added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { editor } from 'monaco-editor';
+
+// tslint:disable-next-line no-any
+declare const monaco: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iomete-editor';
+  editor?: editor.ICodeEditor | editor.IEditor;
+  code = `import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor'`;
+
+  // https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditorconstructionoptions.html
+  defaultEditorOption = {
+    acceptSuggestionOnEnter: "on",// | "smart" | "off",
+    cursorBlinking: "blink",// | "smooth" | "phase" | "expand" | "solid",
+    cursorStyle: "line",// | "block" | "underline" | "line-thin" | "block-outline" | "underline-thin"
+    language: "typescript"
+  }
+
+
+
+  onEditorInit(e: editor.ICodeEditor | editor.IEditor): void {
+    console.log(e);
+    
+    this.editor = e;
+    // this.editor.setModel(monaco.editor.createModel("console.log('Hello ng-zorro-antd')", 'typescript'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor';
+import { NzConfigService } from 'ng-zorro-antd/core/config/public-api';
+
+import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
+import { NzButtonModule, NzGridModule, NzCardModule } from 'ng-zorro-antd/';
+
+const ngZorroConfig: NzConfig = {
+  message: { nzTop: 120 },
+  notification: { nzTop: 240 },
+  
+};
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +31,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzCodeEditorModule,
+    NzGridModule,
+    NzButtonModule,
+    NzCardModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_CONFIG, useValue: ngZorroConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
