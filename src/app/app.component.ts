d4b857170c5e93c96090037dafbc04f801a3026a
vugar.dadalov <vugar.dadalov@gmail.com>
2021-01-20 11:43:25 +0400

    setSuggestion test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,18 +22,59 @@ export class AppComponent {
     acceptSuggestionOnEnter: "on",// | "smart" | "off",
     cursorBlinking: "blink",// | "smooth" | "phase" | "expand" | "solid",
     cursorStyle: "line",// | "block" | "underline" | "line-thin" | "block-outline" | "underline-thin"
-    language: "sql"//json
+    language: "sql"//json, sql
   }
 
-  constructor(private utilService: UtilService) {}
+  constructor(private utilService: UtilService) { }
 
   onEditorInit(e: editor.ICodeEditor | editor.IEditor): void {
     this.editor = e;
-    console.log(monaco.languages);
-    console.log(e);
-
+    // console.log(monaco.languages);
+    // console.log(e);
+    // this.setSuggestion()
     // this.applyCustomSql();
     // this.editor.setModel(monaco.editor.createModel("console.log('Hello ng-zorro-antd')", 'typescript'));
+  }
+
+  setSuggestion() {
+    monaco.languages.register({ id: 'sql' });
+    monaco.languages.registerCompletionItemProvider('sql', {
+      triggerCharacters:["."],
+			autoIndent:true,
+      provideCompletionItems: (model, position) => {
+        var word = model.getWordUntilPosition(position);
+        var range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,// - 1, // UNCOMMENT THIS AND IT BREAKS
+          endColumn: word.endColumn
+      };
+        var suggestions = [
+          {
+            label: 'call_center',
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            insertText: 'call_center',
+            documentation:'Table',
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range
+          },
+          {
+            label: 'testing',
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: 'testing(${1:condition})',
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          },
+          {
+            label: 'ifelse',
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            insertText: ['if (${1:condition}) {', '\t$0', '} else {', '\t', '}'].join('\n'),
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            documentation: 'If-Else Statement',
+          },
+        ];
+        return { suggestions: suggestions };
+      },
+    });
   }
 
   getData(): void {
@@ -70,7 +111,7 @@ export class AppComponent {
   }
 
   // For custom language
-  applyCustomSql(){
+  applyCustomSql() {
     monaco.languages.register({ id: 'custom-sql' });
     monaco.languages.setLanguageConfiguration('custom-sql', conf);
     monaco.languages.setMonarchTokensProvider('custom-sql', language);
