e4ad8cc9f6884387cfbbdb7f5578b5bf7f6fd11f
vugar.dadalov <vugar.dadalov@gmail.com>
2021-04-27 22:11:18 +0400

    sql viewer fix

--- src/app/sql-viewer/sql-viewer.component.ts
+++ src/app/sql-viewer/sql-viewer.component.ts
@@ -14,10 +14,21 @@ export class SqlViewerComponent implements OnInit {
 
   // https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditorconstructionoptions.html
   defaultEditorOption = {
-    acceptSuggestionOnEnter: "on",// | "smart" | "off",
-    cursorBlinking: "blink",// | "smooth" | "phase" | "expand" | "solid",
-    cursorStyle: "line",// | "block" | "underline" | "line-thin" | "block-outline" | "underline-thin"
+    // acceptSuggestionOnEnter: "on",// | "smart" | "off",
+    cursorBlinking: "solid",// | "smooth" | "phase" | "expand" | "solid","blink"
+    cursorStyle: "line-thin",// "line"| "block" | "underline" | "line-thin" | "block-outline" | "underline-thin"
     language: "sql",//json, sql
+    minimap: {
+      enabled: false,
+    },
+    readOnly: true,
+    scrollbar: {
+      verticalScrollbarSize: 6,
+      horizontalScrollbarSize: 6,
+    },
+    lineNumbers: false,
+    folding:false
+    // fontSize: 10
   }
 
   constructor() { }
